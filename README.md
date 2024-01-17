70277a03422438a4cc412de8a089edecdf900c3c
joselevelsup <joselevelsup@gmail.com>
2024-01-17 17:24:48 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Express Warmup
+
+## Instructions
+- Fix any Errors that are occur on the server
+- Make sure that the server is listening on the proper port
+- Create a new route to serve up some data to the client
+
+Further instructions are comments in the code.
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+//There seems to be a problem with this route's logic.
+//Everytime I go to http://localhost:8080/html there is an error or it's just not working
+//I want to send a html page but the response param is not doing it's job.
+//Can you figure out why and fix it?
+app.get("/html", function (res, req) {
+  res.sendFile(__dirname + "index.html");
+});
+
+//Create a route that sends a list of Places to the client as json
+const places = [
+  {
+    name: "Marky's Place",
+    address: "123 Main St",
+  },
+];
+
+app.listen(function () {
+  console.log("Server is listening at http://localhost:8080");
+});
